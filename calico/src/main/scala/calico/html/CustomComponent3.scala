87d114377c2256c4dacfaa8241d5349bbb690490
Areeb <011235813phantom@gmail.com>
2025-04-15 01:05:34 +0530

    A very early prototype in the direction of improved DSL

--- calico/src/main/scala/calico/html/CustomComponent3.scala
+++ calico/src/main/scala/calico/html/CustomComponent3.scala
@@ -1,12 +1,32 @@
+/*
+ * Copyright 2022 Arman Bilge
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package calico
 package html
 
 import cats.effect.IO
-import fs2.{Pipe, Stream}
-import fs2.concurrent.Signal
-import org.scalajs.dom.{document, Event, HTMLElement, HTMLInputElement}
 import cats.effect.unsafe.implicits.global
 import cats.syntax.all.*
+import fs2.Pipe
+import fs2.Stream
+import fs2.concurrent.Signal
+import org.scalajs.dom.Event
+import org.scalajs.dom.HTMLElement
+import org.scalajs.dom.HTMLInputElement
+import org.scalajs.dom.document
 
 object CustomComponent {
 
--- sandbox/src/main/scala/calico/Sandbox.scala
+++ sandbox/src/main/scala/calico/Sandbox.scala
@@ -1,13 +1,32 @@
+/*
+ * Copyright 2022 Arman Bilge
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import calico.*
 import calico.html.CustomComponent.{*, given}
-import calico.unsafe.given
 import calico.syntax.*
+import calico.unsafe.given
 import cats.effect.*
+import cats.syntax.all.*
 import fs2.*
 import fs2.concurrent.*
 import fs2.dom.*
-import org.scalajs.dom.{document, Event, HTMLElement, HTMLInputElement}
-import cats.syntax.all.*
+import org.scalajs.dom.Event
+import org.scalajs.dom.HTMLElement
+import org.scalajs.dom.HTMLInputElement
+import org.scalajs.dom.document
 
 val placeholderAttr = Attribute[ccinputElem, String]("placeholder")
 val labelTextAttr = Attribute[cclabelElem, String]("textContent")
